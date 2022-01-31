e813fec0b6dfb5033903cc03249ac5569c1e522d
Lictor Guzman <lctrgzmn@gmail.com>
2022-01-31 13:05:00 -0600

    TODO: driver for running inference engine

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+mod envr;
+mod expr;
+mod infer;
+mod literal;
+mod name;
+mod subst;
+mod types;
+
+pub trait Hashy: Clone + Eq + std::hash::Hash {}
+
+impl<T> Hashy for T where T: Clone + Eq + std::hash::Hash {}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+        println!("{}", std::mem::size_of::<u32>());
+        println!("{}", std::mem::size_of::<i64>())
+    }
+}
