671ac89ec4a380dc6a9dc605d10506c1cd1ba119
Lictor Guzman <lctrgzmn@gmail.com>
2022-02-03 10:43:02 -0600

    simple inference driver

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,85 @@
-mod constraint;
-mod envr;
-mod expr;
-mod failure;
-mod infer;
-mod literal;
-mod name;
-mod subst;
-mod types;
-mod unify;
+pub mod constraint;
+pub mod envr;
+pub mod expr;
+pub mod failure;
+pub mod infer;
+pub mod literal;
+pub mod name;
+pub mod pretty;
+pub mod subst;
+pub mod types;
+pub mod unify;
 
 pub trait Hashy: Clone + Eq + std::hash::Hash {}
 
 impl<T> Hashy for T where T: Clone + Eq + std::hash::Hash {}
+
+pub use name::display_fresh;
+
+pub use constraint::Constraint;
+pub use failure::{Failure, Solve};
+pub use literal::*;
+pub use name::Fresh;
+pub use name::Name as Identifier;
+use subst::Substitutable;
+pub use unify::Unifier;
+
+pub type Constraints = Vec<Constraint>;
+
+#[derive(Clone, Debug)]
+pub struct Inferred {
+    pub expr: expr::Expr,
+    pub constraints: Constraints,
+    pub subst: subst::Subst,
+    pub tipo: types::Type,
+    pub scheme: types::Scheme,
+    pub engine: infer::Infer,
+}
+
+impl Inferred {
+    pub fn get_count(&self) -> u32 {
+        self.engine.count
+    }
+    pub fn get_subst(&self) -> &subst::Subst {
+        &self.subst
+    }
+    pub fn get_constraints(&self) -> &[Constraint] {
+        &self.constraints[..]
+    }
+    pub fn apply_sub(&self) -> types::Type {
+        self.tipo.apply(self.get_subst())
+    }
+    pub fn resolve(self) -> Solve<Inferred> {
+        let constraints = self.constraints;
+        let subst = Unifier::solve(self.subst, &constraints[..])?;
+        let tipo = self.tipo.apply(&subst);
+        let scheme = self.scheme.apply(&subst);
+        let expr = self.expr;
+        let engine = self.engine;
+        Ok(Inferred {
+            subst,
+            tipo,
+            scheme,
+            constraints,
+            expr,
+            engine,
+        })
+    }
+}
+
+pub fn run_inference<X: Into<expr::Expr>>(ex: X) -> Solve<Inferred> {
+    let expr: expr::Expr = ex.into();
+    let mut engine = infer::Infer::new();
+    let (constraints, subst, tipo, scheme) = engine.constraints_expr(&expr)?;
+    Ok(Inferred {
+        expr,
+        constraints,
+        subst,
+        tipo,
+        scheme,
+        engine,
+    })
+}
 
 #[cfg(test)]
 mod tests {
